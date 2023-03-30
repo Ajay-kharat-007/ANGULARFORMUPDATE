3dd54baa5091d8af0059aeb86badbcbcc83d3926
Ajay-kharat-007 <kharatajay023@gmail.com>
2023-03-30 22:19:53 +0530

    some change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ANGULARFORMUPDATE';
+
+  signupForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
+    age : new FormControl('', [Validators.required, Validators.min(18), Validators.max(60)]),
+    phone: new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(12)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    id: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(15)]),
+    password: new FormControl('', [Validators.required])
+  })
+
+  constructor() { }
+
+  registrForm() {
+    console.log(this.signupForm.value)
+  }
+
+  get f(){
+    console.log(this.signupForm.controls)
+    return this.signupForm.controls;
+  }
+
 }
